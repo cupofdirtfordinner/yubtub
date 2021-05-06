45a7bf049484cc653f10940f7152f370bb35fea8
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2021-05-06 11:23:28 -0500

    Update yubtub.js

--- yubtub.js
+++ yubtub.js
@@ -1,5 +1,2 @@
-var x = document.createElement("INPUT");
-x.setAttribute("type", "input");
-var y = document.createElement("INPUT2");
-y.SetAttribute("type", "input");
-
+var input = document.createElement("INPUT");
+input.setAttribute("type", "input");
